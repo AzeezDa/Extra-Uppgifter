d77564d0bb64f537eff80d22b39bfe9f2c3f768c
NTIAzeezDaoud <55975236+NTIAzeezDaoud@users.noreply.github.com>
2020-11-10 13:09:56 +0100

    add uppgift 3

---
+++ WPF Uppgift 3/WPF Uppgift 3/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Uppgift_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            transbutton.Click += translate;
+
+        }
+
+        private void translate(object sender, RoutedEventArgs e)
+        {
+            textbox.Text = textbox.Text.ToLower().Replace("u", "ö");
+            textbox.Text = textbox.Text.ToLower().Replace("o", "u");
+            textbox.Text = textbox.Text.ToLower().Replace("s", "sh");
+            textbox.Text = textbox.Text.ToLower().Replace("r", "rrr");
+            textbox.Text = textbox.Text.ToLower().Replace("t", "ta");
+            textbox.Text = textbox.Text.ToLower().Replace(".", ", arrrrr!");
+
+        }
+    }
+}
---
+++ WPF Uppgift 3/WPF Uppgift 3/MultiLineTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPF_Uppgift_3
+{
+    class MultiLineTextBox : TextBox
+    {
+        public MultiLineTextBox()
+        {
+            FontSize = 15.5;
+            Background = Brushes.Black;
+            Foreground = Brushes.LightGreen;
+            FontFamily = new FontFamily("Courier New");
+            TextWrapping = System.Windows.TextWrapping.Wrap;
+        }
+    }
+}
