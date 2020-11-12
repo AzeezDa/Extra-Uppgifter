beb04be924bd3ad8de9b2c2c2103604d87192a24
NTIAzeezDaoud <55975236+NTIAzeezDaoud@users.noreply.github.com>
2020-11-12 17:51:40 +0100

    Add monogame project of knightdragon

---
+++ Some Knights and a Dragon/Some Knights and a Dragon/Creatures/Creature.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Some_Knights_and_a_Dragon.Creatures
+{
+    abstract class Creature
+    {
+        Texture2D sprite;
+
+        public Creature()
+        {
+        }
+    }
+}
---
+++ Some Knights and a Dragon/Some Knights and a Dragon/Windows/GameWindow.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Some_Knights_and_a_Dragon
+{
+    abstract class GameWindow
+    {
+        public virtual void LoadContent() // Loads the content of the 
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime) // Updates the window content
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch _spriteBatch) // Draw the window content
+        {
+
+        }
+    }
+}
